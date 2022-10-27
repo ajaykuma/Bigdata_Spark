40692e2ec35fd9fc0d717717de55f2f396b20dbb
Aj <ajaykuma24@gmail.com>
2022-10-27 19:05:20 +0200

    updated

--- Pyspark/Project1/dependencies/spark.py
+++ Pyspark/Project1/dependencies/spark.py
@@ -86,7 +86,7 @@ def start_spark(app_name='my_spark_app',master='local[*]',jar_packages=[],
 
     # create session and retrieve Spark logger object
     spark_session = spark_builder.getOrCreate()
-    spark_logger = logging.Log4j(spark_sess)
+    spark_logger = logging.Log4j(spark_session)
 
     # get config file if sent to cluster with --files
     spark_files_dir = SparkFiles.getRootDirectory()
@@ -103,4 +103,4 @@ def start_spark(app_name='my_spark_app',master='local[*]',jar_packages=[],
         spark_logger.warn('no config file found')
         config_dict = None
 
-    return spark_sess, spark_logger, config_dict
+    return spark_session, spark_logger, config_dict
